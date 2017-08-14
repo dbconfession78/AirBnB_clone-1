07e3f65575d318cfbcd39e7505cfd2857a116385
NamoDawn <naomisorrell@gmail.com>
2017-08-14 23:14:59 +0000

    model files with adding in specs for AirBnb

--- models/review.py
+++ models/review.py
@@ -6,12 +6,12 @@ Review Class from Models Module
 from models.base_model import BaseModel
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """Review class handles all application reviews"""
-
-    place_id = ''
-    user_id = ''
-    text = ''
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), nullable=False, ForeignKey('places.id'))
+    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
 
     def __init__(self, *args, **kwargs):
         """instantiates a new review"""
