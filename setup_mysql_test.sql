db2783a32cc44413add8c51e0d62755589a333f9
NamoDawn <naomisorrell@gmail.com>
2017-08-14 18:09:47 +0000

    creation of mysql set up test file and modification to dev mysql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- sets up a new test db and user for airbnb site
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+GRANT ALL ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
