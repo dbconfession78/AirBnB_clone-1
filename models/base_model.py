7c63f54452239715a10d9ae2c8fe269cff2bbc84
Stuart Kuredjian <108@holbertonschool.com>
2017-08-15 01:01:59 +0000

    /base_model.py :added some attributes for db mapping

--- models/base_model.py
+++ models/base_model.py
@@ -8,14 +8,20 @@ import models
 from uuid import uuid4, UUID
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
 
-
+Base = declarative_base()
 now = datetime.now
 strptime = datetime.strptime
 
 
 class BaseModel():
     """attributes and functions for BaseModel class"""
+
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(datetime, nullable=False, default=datetime.utcnow())
+    update_at = Column(datetime, nullable=False, default=datetime.utcnow())
+    
 
     def __init__(self, *args, **kwargs):
         """instantiation of new BaseModel Class"""
@@ -24,42 +30,7 @@ class BaseModel():
         else:
             self.id = str(uuid4())
             self.created_at = now()
-            models.storage.new(self)
 
-#        self.created_at = now()
-#        self.id = str(uuid4())
-#        if(kwargs):
-#            d = self.__dict__
-#            self.created_at = now()
-#            self.__set_attributes(d, kwargs)
-#            models.storage.new(self)
-#        else:
-#            self.created_at = now()
-#            models.storage.new(self)
-
-    @classmethod
-    def __set_date_time(self, d, dt_key):
-        if 'update_at' in d:
-            d[dt_key] = strptime(d['updated_at'],
-                                 "%Y-%m-%d %H:%M:%S.%f")
-        return d
-
-    def __set_attributes_NEW(self, d, kwargs):
-        if 'created_at' in d:
-            d = self.__set_date_time(d, 'created_at')
-        if 'updated_at' in d:
-            d = self.__set_date_time(d, 'updated_at')
-
-        for k, v in d.items():
-            d[k] = v
-        for k, v in kwargs.items():
-            d[k] = v
-
-        if '__class__' in d:
-            d.pop('__class__')
-        self.__dict__ = d
-
-    # TO DELETE
     def __set_attributes(self, d):
         """converts kwargs values to python class attributes"""
         if not isinstance(d['created_at'], datetime):
@@ -87,6 +58,7 @@ class BaseModel():
     def save(self):
         """updates attribute updated_at to current time"""
         self.updated_at = now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_json(self):
@@ -98,6 +70,9 @@ class BaseModel():
             else:
                 bm_dict[k] = str(v)
         bm_dict["__class__"] = type(self).__name__
+        if '_sa_instance_state' in bm_dict:
+            del bm_dict['_sa_instance_state']
+
         return(bm_dict)
 
     def __str__(self):
