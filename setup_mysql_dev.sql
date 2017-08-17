600ebc780a3bade39c58f08e8d973a5213402d5e
Stuart Kuredjian <108@holbertonschool.com>
2017-08-17 01:08:08 +0000

    add FLUSH PRIVILEGES to both files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,3 +5,4 @@ USE hbnb_dev_db;
 GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES
