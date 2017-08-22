7ef51847b5eb8d3e1b7650e42588a97c3550c660
Stuart Kuredjian <108@holbertonschool.com>
2017-08-22 02:10:53 +0000

    1-pack_web_static.py :initial commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 1-pack_web_static
+"""
+from fabric.api import *
+import time
+
+def do_pack():
+    """
+    packages the web_static folders contents into a tar (.tgz)
+    """
+    try:
+        now = time.strftime("%Y%m%d%H%M%S")
+        local('mkdir -p ./versions')
+        local('tar -cvzf versions/web_static_{}.tgz web_static'.format(now))
+        return("versions/web_static_{}.tgz".format(now))
+    except:
+        return None
