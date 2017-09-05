bf221ef6463ae0d0f2272dad3fe97b676dd1fb16
Stuart Kuredjian <108@holbertonschool.com>
2017-09-05 18:41:45 +0000

    0-hello_route.py :initial commit

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+0-hell_route.py - starts a Flask web app
+"""
+from flask import Flask
+app = Flask('__name__')
+
+
+@app.route('/')
+def hello_hbnb():
+    app.url_map.strict_slashes = False
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
