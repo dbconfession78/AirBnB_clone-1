478fbe1ebab65148d6dd13e9ebd3de09823d7c0d
Stuart Kuredjian <108@holbertonschool.com>
2017-09-05 20:36:53 +0000

    2-c_route.py :replace underscores with a space

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+1-hbnb_route.py - starts a Flask web application
+"""
+from flask import Flask
+app = Flask('__name__')
+
+
+@app.route('/')
+def hello_hbnb():
+    """ outputs 'Hello HBNB!' """
+    app.url_map.strict_slashes = False
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ outputs 'HBNB' """
+    app.url_map.strict_slashes = False
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ outputs 'C <text> """
+    text = text.replace('_', ' ')
+
+    app.url_map.strict_slashes = False
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
