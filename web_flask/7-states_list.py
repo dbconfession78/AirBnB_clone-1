e8d76147a8d2622957399cf6e0f4e72acb0fcd62
Stuart Kuredjian <108@holbertonschool.com>
2017-09-07 02:26:04 +0000

    revert

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application
+7-states_list - starts a flask application
 """
-
-
-from models import storage
 from flask import Flask, render_template
+from models import storage
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown(self):
-    """ removes current SQLAlchemy Session """
+    """  close storage session """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states_list')
 def states_list():
-    """ displays 7-states_list.html """
-    states = storage.all("State").values()
-    return render_template('7-states_list.html', states=states)
+    """ injects states list into html  """
+    app.url_map.strict_slashes = False
+    states = storage.all('State').values()
+    return (render_template('7-states_list.html', states=states))
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
