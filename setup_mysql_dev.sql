553c508689c81113cc06df9d1a6ba842c53a67ae
Stuart Kuredjian <108@holbertonschool.com>
2017-09-07 03:57:00 +0000

    update to use mysql 5.7 synatx

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,9 @@
--- sets up a new db and user for airbnb site
-
+-- creates MySQL database hbnb_dev_db only if not existing
+-- and gives privileges to user hbnb_dev on 2 DB's
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-USE hbnb_dev_db;
-GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+      TO hbnb_dev@localhost
+      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.*
+      TO hbnb_dev@localhost
+      IDENTIFIED BY 'hbnb_dev_pwd';
