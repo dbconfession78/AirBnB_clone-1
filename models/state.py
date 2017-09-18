310c9a35bd080261b97141ac0b477c88b813fad4
Stuart Kuredjian <108@holbertonschool.com>
2017-09-18 18:17:28 +0000

    revert to my passing version

--- models/state.py
+++ models/state.py
@@ -3,54 +3,42 @@
 State Class from Models Module
 """
 
-# from models.base_model import BaseModel, Base
-# import models
-# from sqlalchemy import String, Column
-# from sqlalchemy.orm import relationship
-# from os import environ, getenv
-
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-# from sqlalchemy.orm import *
-from sqlalchemy import *
-from os import getenv, environ
+import models
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+from os import environ, getenv
 
 
 class State(BaseModel, Base):
     """State class handles all application states"""
 
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
+    if 'HBNB_TYPE_STORAGE' in environ and environ['HBNB_TYPE_STORAGE'] == 'db':
+
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        #        cities = relationship("City", cascade="all, delete-orphan",
-        #                             backref="state")
-        cities = relationship('City', backref='state', cascade="all")
+        cities = relationship("City", cascade="all, delete-orphan",
+                              backref="state")
     else:
         name = ""
 
-        #    if (getenv('HBNB_TYPE_STORAGE') != 'db'):
+    if (getenv('HBNB_TYPE_STORAGE') != 'db'):
         @property
         def cities(self):
             """ returns all city objects  associated with this State  """
 
-            #  cities = models.storage.all('City').values()
-
-            cities_list = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+            cities = models.storage.all('City').values()
 
             # this one line does all of the commented
-#            results = [city for city in cities if city.state_id == self.id]
+            results = [city for city in cities if city.state_id == self.id]
 
 #            results = []
 #            for city in cities:
 #                if (city.state_id == self.id):
 #                    results.append(city)
 
-#           return (results)
+            return (results)
 
-#    def __init__(self, *args, **kwargs):
-#       """instantiates a new state"""
-#      super().__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """instantiates a new state"""
+        super().__init__(self, *args, **kwargs)
