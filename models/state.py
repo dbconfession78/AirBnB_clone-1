2ee977de34aac259aeca39e8c631b0e38e9f7c90
Stuart Kuredjian <108@holbertonschool.com>
2017-09-18 17:03:36 +0000

    revert

--- models/state.py
+++ models/state.py
@@ -2,30 +2,42 @@
 """
 State Class from Models Module
 """
-from sqlalchemy import Column, String
-from sqlalchemy import *
-from sqlalchemy.orm import *
+
 from models.base_model import BaseModel, Base
-from os import getenv, environ
+import models
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+from os import environ, getenv
 
 
 class State(BaseModel, Base):
     """State class handles all application states"""
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
+
+    if 'HBNB_TYPE_STORAGE' in environ and environ.get('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade="all")
+        cities = relationship("City", cascade="all, delete-orphan",
+                              backref="state")
     else:
-        name = ''
+        name = ""
 
+    if (getenv('HBNB_TYPE_STORAGE') != 'db'):
         @property
         def cities(self):
-            """
-                getter method, returns list of City objs from storage
-                linked to the current State
-            """
-            cities_list = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+            """ returns all city objects  associated with this State  """
+
+            cities = models.storage.all('City').values()
+
+            # this one line does all of the commented
+            results = [city for city in cities if city.state_id == self.id]
+
+#            results = []
+#            for city in cities:
+#                if (city.state_id == self.id):
+#                    results.append(city)
+
+            return (results)
+
+    def __init__(self, *args, **kwargs):
+        """instantiates a new state"""
+        super().__init__(self, *args, **kwargs)
