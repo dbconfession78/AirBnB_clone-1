8a96952c6a1c62ccc2eb08759a24ba70c5af2c16
Stuart Kuredjian <108@holbertonschool.com>
2017-09-18 17:00:23 +0000

    testing Julia's state.py

--- models/state.py
+++ models/state.py
@@ -2,42 +2,30 @@
 """
 State Class from Models Module
 """
-
+from sqlalchemy import Column, String
+from sqlalchemy import *
+from sqlalchemy.orm import *
 from models.base_model import BaseModel, Base
-import models
-from sqlalchemy import String, Column
-from sqlalchemy.orm import relationship
-from os import environ, getenv
+from os import getenv, environ
 
 
 class State(BaseModel, Base):
     """State class handles all application states"""
-
-    if 'HBNB_TYPE_STORAGE' in environ and environ['HBNB_TYPE_STORAGE'] == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete-orphan",
-                              backref="state")
+        cities = relationship('City', backref='state', cascade="all")
     else:
-        name = ""
+        name = ''
 
-    if (getenv('HBNB_TYPE_STORAGE') != 'db'):
         @property
         def cities(self):
-            """ returns all city objects  associated with this State  """
-
-            cities = models.storage.all('City').values()
-
-            # this one line does all of the commented
-            results = [city for city in cities if city.state_id == self.id]
-
-#            results = []
-#            for city in cities:
-#                if (city.state_id == self.id):
-#                    results.append(city)
-
-            return (results)
-
-    def __init__(self, *args, **kwargs):
-        """instantiates a new state"""
-        super().__init__(self, *args, **kwargs)
+            """
+                getter method, returns list of City objs from storage
+                linked to the current State
+            """
+            cities_list = []
+            for city in models.storage.all("City").values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
